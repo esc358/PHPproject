92881894ef59e2bdfecc61dc639d3e57e087a41a
esc358 <200478144@student.georgianc.on.ca>
2021-07-11 19:23:04 -0400

    Created file register and validations and gitignore

---
+++ validations.php
@@ -0,0 +1,33 @@
+<?php
+
+function validate_registration($user, $conn){
+    $errors = [];
+
+        if(empty(trim($user['email']))){
+            $errors['email'] = "Email cannot be blank";
+        }
+
+        $email_regex = "/.+\@.+\..+/";
+        if(!preg_match($email_regex, $user['email'])){
+            $errors['email'] = "Username email must be valid";
+        }
+
+        if(empty(trim($user['new-password']))){
+            $errors['password'] = "Password cannot be blank";
+        }
+
+        if(empty(trim($user['confirm-password']))){   
+            $errors['confirm'] = "Confirm password cannot be blank";
+        }
+
+        $sql = "SELECT * FROM users_dota WHERE username='" . $user['email'] . "'";
+        $cmd = $conn -> prepare($sql);
+        $cmd -> execute();
+        $found_username = $cmd -> fetch();
+
+        if($found_username){
+            $errors['email'] = 'Username already taken';
+        }
+   
+    return $errors;
+}
