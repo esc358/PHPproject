b708f8be09ba2da2784de7bc5c8569e512d77747
esc358 <200478144@student.georgianc.on.ca>
2021-07-15 21:17:03 -0400

    added functions to validations

--- validations.php
+++ validations.php
@@ -1,5 +1,19 @@
 <?php
 
+function is_logged_in()
+{
+    return isset($_SESSION['user_id']);
+}
+
+function require_login()
+{
+    if(!is_logged_in())
+    {
+        header("Location:login.php");
+        exit;
+    }
+}
+
 function validate_registration($user, $conn){
     $errors = [];
 
